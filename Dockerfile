bfc01f3c8582e2e3302bf8ebff79eedf9501f37a
PIAAR <piaar@rmasters.group>
2022-09-11 10:35:59 -0400

    update configg

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.8
 
-MAINTAINER Part Time Larry "parttimelarry@gmail.com"
+LABEL RMG "piaar@rmasters.group"
 
 ADD . /app
 
---
+++ backtests/strategy.py
@@ -0,0 +1,30 @@
+# RMG strategy is to:
+# - run a bot that will trade STOCKS, CRYPTO, and FOREX
+# STOCKS - 
+#   from Bianance
+#   from Alpaca
+#   from Yfinance
+# 
+
+from tracemalloc import start
+from typing_extensions import Self
+
+
+Class Strategy(self) as start:
+    
+    global = self.global
+    
+    def stocks(self):
+        pass
+
+    def crypto(self):
+        pass
+    
+    def forex(self):
+        pass
+
+
+
+
+
+
